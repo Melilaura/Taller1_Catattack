a412553104e7507a488e6b88f0cdec1fd312832d
Chane <orozcoveralauramelissa@gmail,com>
2021-03-04 08:56:17 -0500

    primer commit

---
+++ src/Principal.java
@@ -0,0 +1,34 @@
+import processing.core.PApplet;
+
+public class Principal extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PApplet.main("Principal");		
+			
+	}
+	
+	@Override
+	public void settings() {
+	 size(500, 500);
+	 
+	}
+	
+
+
+	@Override
+	public void setup() {
+				
+	}
+	
+	
+	@Override
+	public void draw() {
+		
+		
+	}
+	
+	
+
+}
