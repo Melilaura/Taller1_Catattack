f3b03f0bc5ab353a48ef046cd9c2c711b5e8c1a5
Chane <orozcoveralauramelissa@gmail,com>
2021-03-04 09:34:37 -0500

    se dibujan las imagenes de los fondos

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Principal extends PApplet{
 
@@ -15,17 +16,93 @@ public class Principal extends PApplet{
 	 
 	}
 	
+	// wall
+	PImage instructions;
+	PImage menu;
+	PImage resume;
+	PImage game;
+	
+	// characters
+	// hero
+	PImage hero;
+	PImage herodead;
+	
+	// cats
+	
+	PImage cat1;
+	PImage cat1dead;
+	
+	PImage cat2;
+	PImage cat2dead;
+	
+	PImage cat3;
+	PImage cat3dead;
+	
+	// bullet
+	
+	PImage bullet;
+	
+	
+	//botones
+	
+	
+
 
 
 	@Override
 	public void setup() {
-				
+		
+		// load images wall
+		instructions = loadImage("instrucciones.png");
+		menu = loadImage("menu.png");
+		resume = loadImage("resume.png");
+		game = loadImage("juego.png");
+		
+		// load images characters
+		// hero
+		hero= loadImage("hero1.png");
+		herodead= loadImage("hero2.png");
+		
+		// load images cats
+		
+		cat1 = loadImage("cat1.png");
+		cat1dead = loadImage("cat1b.png");
+		
+		cat2 = loadImage("cat2.png");
+		cat2dead = loadImage("cat2b.png");
+		
+		cat3 = loadImage("cat3.png");
+		cat3dead = loadImage("cat3b.png");
+		
+		// load image bullet
+		
+		bullet = loadImage("bullet.png") ;
 	}
 	
 	
 	@Override
 	public void draw() {
 		
+		background(74, 86, 71);
+		
+		//imageMode(CENTER);
+		//image(menu, 576, 350);
+		//imageMode(CORNER); 
+		
+		//imageMode(CENTER);
+		//image(instructions, 576, 350);
+		//imageMode(CORNER);
+		
+		
+		
+		//imageMode(CENTER);
+		//image(game, 576, 350);
+		//imageMode(CORNER);
+		
+		
+		imageMode(CENTER);
+		image(resume, 576, 350);
+		imageMode(CORNER);
 		
 	}
 	
