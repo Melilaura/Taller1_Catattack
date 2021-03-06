325f70f4e5355f11e640611fcd9604be18428e05
Chane <orozcoveralauramelissa@gmail,com>
2021-03-06 02:28:53 -0500

    se edito la clase bullet

--- src/Bullet.java
+++ src/Bullet.java
@@ -3,4 +3,33 @@ import processing.core.PImage;
 
 public class Bullet {
 
+	private int posX, posY;
+	private PApplet app;
+
+	public PImage bullet;
+	
+
+	public Bullet (PApplet app, int posX, int posY) {
+		this.posX = posX;
+		this.posY = posY;
+		
+		bullet= app.loadImage("bullet.png");
+		
+	}
+
+
+	public void drawBala(PApplet app) {
+
+		//bullet.resize(0, 200);
+		app.imageMode(app.CENTER);
+		app.image(bullet, posX, posY);
+		app.imageMode(app.CORNER);
+	}
+	
+	public void moverBala(int tanqueX, int tanqueY) {
+
+		posY -= 5;
+
+	}
+
 }
